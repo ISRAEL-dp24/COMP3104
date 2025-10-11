47a72f823076570b1b8ae20be983d6807ae5f6be
ISRAEL_dp24 <dolapoosunkoya5@gmail.com>
2025-10-11 02:29:33 -0400

    Name change

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101499955 || Israel Osunkoya || DevOps</h5>
+      <h5>Developed By: 101499955 || Israel Osunkoya || COMP3104 ||DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
